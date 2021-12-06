210c994827074531e9f3f5a9fb52ce89ab0f619c
andrewmarkdale <andrew.dale@ontariotechu.net>
2021-12-05 23:42:21 -0500

    Final push to this repo

--- vscanner_finalproject_csci4100/lib/model/vege_check_functions.dart
+++ vscanner_finalproject_csci4100/lib/model/vege_check_functions.dart
@@ -3,11 +3,13 @@ import 'package:flutter/material.dart';
 
 // These important functions check which icon to use for the various pages.
 String veganCheck(Product product) {
-  if (product.vegan == "yes") {
+  if (product.vegan == "yes" || product.vegan == "images/vegan_icon_true.png") {
     return "images/vegan_icon_true.png";
-  } else if (product.vegan == "maybe-vegan") {
+  } else if (product.vegan == "maybe-vegan" ||
+      product.vegan == "images/vegan_icon_maybe.png") {
     return "images/vegan_icon_maybe.png";
-  } else if (product.vegan == "non-vegan") {
+  } else if (product.vegan == "non-vegan" ||
+      product.vegan == "images/vegan_icon_false.png") {
     return "images/vegan_icon_false.png";
   } else {
     return "images/vegan_icon_false_unknown.png";
@@ -15,11 +17,11 @@ String veganCheck(Product product) {
 }
 
 String strVeganCheck(String vegan) {
-  if (vegan == "yes") {
+  if (vegan == "yes" || vegan == "images/vegan_icon_true.png") {
     return "images/vegan_icon_true.png";
-  } else if (vegan == "maybe-vegan") {
+  } else if (vegan == "maybe-vegan" || vegan == "images/vegan_icon_maybe.png") {
     return "images/vegan_icon_maybe.png";
-  } else if (vegan == "non-vegan") {
+  } else if (vegan == "non-vegan" || vegan == "images/vegan_icon_false.png") {
     return "images/vegan_icon_false.png";
   } else {
     return "images/vegan_icon_false_unknown.png";
@@ -27,11 +29,14 @@ String strVeganCheck(String vegan) {
 }
 
 String vegetarianCheck(Product product) {
-  if (product.vegetarian == "yes") {
+  if (product.vegetarian == "yes" ||
+      product.vegetarian == "images/vegetarian_icon_true.png") {
     return "images/vegetarian_icon_true.png";
-  } else if (product.vegetarian == "maybe-vegetarian") {
+  } else if (product.vegetarian == "maybe-vegetarian" ||
+      product.vegetarian == "images/vegetarian_icon_maybe.png") {
     return "images/vegetarian_icon_maybe.png";
-  } else if (product.vegetarian == "non-vegetarian") {
+  } else if (product.vegetarian == "non-vegetarian" ||
+      product.vegetarian == "images/vegetarian_icon_false.png") {
     return "images/vegetarian_icon_false.png";
   } else {
     return "images/vegetarian_icon_unknown.png";
@@ -39,11 +44,13 @@ String vegetarianCheck(Product product) {
 }
 
 String strVegetarianCheck(String vegetarian) {
-  if (vegetarian == "yes") {
+  if (vegetarian == "yes" || vegetarian == "images/vegetarian_icon_true.png") {
     return "images/vegetarian_icon_true.png";
-  } else if (vegetarian == "maybe-vegetarian") {
+  } else if (vegetarian == "maybe-vegetarian" ||
+      vegetarian == "images/vegetarian_icon_maybe.png") {
     return "images/vegetarian_icon_maybe.png";
-  } else if (vegetarian == "non-vegetarian") {
+  } else if (vegetarian == "non-vegetarian" ||
+      vegetarian == "images/vegetarian_icon_false.png") {
     return "images/vegetarian_icon_false.png";
   } else {
     return "images/vegetarian_icon_false.png";
